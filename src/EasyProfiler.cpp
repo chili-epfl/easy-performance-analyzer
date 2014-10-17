b56698cd229dd57068ad4ebf637343009b46728a
ayberkozgur <ayberk.ozgur@epfl.ch>
2014-10-17 13:46:00 +0200

    Offline profiling

--- src/EasyProfiler.cpp
+++ src/EasyProfiler.cpp
@@ -30,6 +30,7 @@
 
 Str2Clk EasyProfiler::blocks;
 Str2SMarker EasyProfiler::smoothBlocks;
+Str2AMarker EasyProfiler::offlineBlocks;
 std::string EasyProfiler::androidTag = "EASYPROFILER";
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -52,7 +53,7 @@ void EasyProfiler::endProfiling(const std::string& blockName)
     if(pairIt == blocks.end())
         EZP_OMNIPRINT("Can't find %s, did you call EZP_START() on %s?\n", blockName.c_str(), blockName.c_str());
     else{
-        EZP_OMNIPRINT("%s took %.2f ms\n",blockName.c_str(),getTimeDiff(pairIt->second,&end));
+        EZP_OMNIPRINT("%s\t%6.2f ms\n",blockName.c_str(),getTimeDiff(pairIt->second,&end));
         blocks.erase(pairIt);
         delete pairIt->second;
     }
@@ -79,9 +80,70 @@ void EasyProfiler::endProfilingSmooth(const std::string& blockName, float sf)
     if(pairIt == smoothBlocks.end())
         EZP_OMNIPRINT("Can't find %s, did you call EZP_START_SMOOTH() on %s?\n", blockName.c_str(), blockName.c_str());
     else{
-        pairIt->second->lastSlice = sf*pairIt->second->lastSlice + (1.0f - sf)*getTimeDiff(&pairIt->second->beginTime,&end);
-        EZP_OMNIPRINT("%s took %.2f ms\n",blockName.c_str(),pairIt->second->lastSlice);
+        pairIt->second->lastSlice = sf*pairIt->second->lastSlice + (1.0f - sf)*getTimeDiff(&(pairIt->second->beginTime),&end);
+        EZP_OMNIPRINT("%s\t%6.2f ms\n",blockName.c_str(),pairIt->second->lastSlice);
     }
+}
+
+void EasyProfiler::startProfilingOffline(const std::string& blockName)
+{
+    //Get time in the very end to disturb the measurements the least possible
+    AggregateMarker* begin = new AggregateMarker();
+    Str2AMarker::iterator pairIt = offlineBlocks.find(blockName);
+    if(pairIt == offlineBlocks.end()){
+        offlineBlocks.insert(Str2AMarkerPair(blockName,begin));
+        clock_gettime(EZP_CLOCK,&(begin->beginTime));
+    }
+    else
+        clock_gettime(EZP_CLOCK,&(pairIt->second->beginTime));
+}
+
+void EasyProfiler::endProfilingOffline(const std::string& blockName)
+{
+    Timespec end;
+    clock_gettime(EZP_CLOCK,&end);
+    Str2AMarker::iterator pairIt = offlineBlocks.find(blockName);
+    if(pairIt == offlineBlocks.end())
+        EZP_OMNIPRINT("Can't find %s, did you call EZP_START_OFFLINE() on %s?\n", blockName.c_str(), blockName.c_str());
+    else{
+        pairIt->second->numSamples++;
+        pairIt->second->totalTime += getTimeDiff(&(pairIt->second->beginTime), &end);
+    }
+}
+
+void EasyProfiler::printOfflineProfiles()
+{
+    if(offlineBlocks.size() == 0){
+        EZP_OMNIPRINT("No offline profile found; profile some code first by wrapping it with EZP_START_OFFLINE() ... EZP_END_OFFLINE()\n");
+        return;
+    }
+
+    //Sort and find max block name length
+    std::vector<AggregateProfile> sortedProfiles(offlineBlocks.size());
+    int maxNameLen = 0;
+    Str2AMarker::iterator its = offlineBlocks.begin();
+    std::vector<AggregateProfile>::iterator itt = sortedProfiles.begin();
+    for(; its != offlineBlocks.end(); its++, itt++){
+        if(its->first.length() > maxNameLen)
+            maxNameLen = its->first.length();
+        itt->blockName = its->first;
+        itt->numSamples = its->second->numSamples;
+        itt->averageTime = its->second->totalTime/itt->numSamples;
+    }
+    std::sort(sortedProfiles.begin(),sortedProfiles.end(),AggregateProfile_::compare);
+
+    //Do the printing
+    std::string spaces(maxNameLen - 4, ' ');
+    std::string dashes(maxNameLen - 4, '-');
+    EZP_OMNIPRINT("%sName    Average(ms)         Total(ms)           Calls     \n", spaces.c_str());
+    EZP_OMNIPRINT("%s----------------------------------------------------------\n", dashes.c_str());
+    for(std::vector<AggregateProfile>::iterator it = sortedProfiles.begin(); it != sortedProfiles.end(); it++)
+        EZP_OMNIPRINT("%*s    %-16.2f    %-16.2f    %-10d\n", maxNameLen, it->blockName.c_str(), it->averageTime, it->averageTime*it->numSamples, it->numSamples);
+}
+
+void EasyProfiler::clearOfflineProfiles()
+{
+    offlineBlocks.clear();
 }
 
 inline float EasyProfiler::getTimeDiff(const Timespec* begin, const Timespec* end)
--- src/EasyProfiler.hpp
+++ src/EasyProfiler.hpp
@@ -28,6 +28,8 @@
 #include<ctime>
 #include<map>
 #include<string>
+#include<vector>
+#include<algorithm>
 
 #ifdef ANDROID
 #include<android/log.h>
@@ -54,6 +56,31 @@ SmoothMarker;
 typedef std::map<std::string,SmoothMarker*> Str2SMarker;
 typedef std::pair<std::string,SmoothMarker*> Str2SMarkerPair;
 
+typedef struct AggregateMarker_
+{
+    Timespec beginTime;
+    float totalTime;
+    int numSamples;
+
+    AggregateMarker_(){ totalTime = 0.0f; numSamples = 0; }
+}
+AggregateMarker;
+
+typedef std::map<std::string,AggregateMarker*> Str2AMarker;
+typedef std::pair<std::string,AggregateMarker*> Str2AMarkerPair;
+
+typedef struct AggregateProfile_
+{
+    std::string blockName;
+    float averageTime;
+    int numSamples;
+
+    static bool compare(const struct AggregateProfile_& one, const struct AggregateProfile_& two){
+        return one.averageTime > two.averageTime;
+    }
+}
+AggregateProfile;
+
 /**
  * @brief Simple instrumentation profiler that relies on CPU clocks
  */
@@ -68,18 +95,18 @@ public:
     static void startProfiling(const std::string& blockName = "UNNAMED_BLOCK");
 
     /**
-     * @brief Starts a named profile that is smoothed over time
-     *
-     * @param blockName Name of the profile
-     */
-    static void startProfilingSmooth(const std::string& blockName = "UNNAMED_BLOCK");
-
-    /**
      * @brief Ends the named profile, it must have been started before
      *
      * @param blockName Name of the profile
      */
     static void endProfiling(const std::string& blockName = "UNNAMED_BLOCK");
+
+    /**
+     * @brief Starts a named profile that is smoothed over time
+     *
+     * @param blockName Name of the profile
+     */
+    static void startProfilingSmooth(const std::string& blockName = "UNNAMED_BLOCK");
 
     /**
      * @brief  Ends a named smoothed profile, it must have been started before
@@ -88,6 +115,15 @@ public:
      * @param smoothingFactor Coefficient of the history, between 0 and 1
      */
     static void endProfilingSmooth(const std::string& blockName = "UNNAMED_BLOCK", float smoothingFactor = 0.95f);
+
+
+    static void startProfilingOffline(const std::string& blockName = "UNNAMED_BLOCK");
+
+    static void endProfilingOffline(const std::string& blockName = "UNNAMED_BLOCK");
+
+    static void printOfflineProfiles();
+
+    static void clearOfflineProfiles();
 
     static std::string androidTag;      ///< Logcat tag on Android
 
@@ -105,6 +141,7 @@ private:
 
     static Str2Clk blocks;              ///< Names and beginning times of profile blocks
     static Str2SMarker smoothBlocks;    ///< Names, beginning times and latest time slices of smoothed profile blocks
+    static Str2AMarker offlineBlocks;   ///< Names, beginning times, total times and number of samples of offline profile blocks
 };
 
 #ifdef ANDROID
@@ -124,14 +161,14 @@ private:
 #define EZP_START(BLOCK_NAME) EasyProfiler::startProfiling(BLOCK_NAME);
 
 /**
- * @brief Alias for EasyProfiler::startProfilingSmooth()
- */
-#define EZP_START_SMOOTH(BLOCK_NAME) EasyProfiler::startProfilingSmooth(BLOCK_NAME);
-
-/**
  * @brief Alias for EasyProfiler::endProfiling()
  */
 #define EZP_END(BLOCK_NAME) EasyProfiler::endProfiling(BLOCK_NAME);
+
+/**
+ * @brief Alias for EasyProfiler::startProfilingSmooth()
+ */
+#define EZP_START_SMOOTH(BLOCK_NAME) EasyProfiler::startProfilingSmooth(BLOCK_NAME);
 
 /**
  * @brief Alias for EasyProfiler::endProfilingSmooth() with default smoothing factor
@@ -139,8 +176,14 @@ private:
 #define EZP_END_SMOOTH(BLOCK_NAME) EasyProfiler::endProfilingSmooth(BLOCK_NAME);
 
 /**
- * @brief Alias for EasyProfiler::endProfilingSmooth() with custom smoothing factor 
+ * @brief Alias for EasyProfiler::endProfilingSmooth() with custom smoothing factor
  */
 #define EZP_END_SMOOTH_FACTOR(BLOCK_NAME,SMOOTHING_FACTOR) EasyProfiler::endProfilingSmooth(BLOCK_NAME,SMOOTHING_FACTOR);
+
+
+#define EZP_START_OFFLINE(BLOCK_NAME) EasyProfiler::startProfilingOffline(BLOCK_NAME);
+#define EZP_END_OFFLINE(BLOCK_NAME) EasyProfiler::endProfilingOffline(BLOCK_NAME);
+#define EZP_PRINT_OFFLINE EasyProfiler::printOfflineProfiles();
+#define EZP_CLEAR_OFFLINE EasyProfiler::clearOfflineProfiles();
 
 #endif /* EASYPROFILER_HPP */
