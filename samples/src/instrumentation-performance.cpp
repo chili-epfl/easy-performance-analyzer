f00178e97d85eba5cdc6fdc7145f1786c24ed331
ayberkozgur <ayberk.ozgur@epfl.ch>
2014-11-02 16:14:28 +0100

    Improved instrumentation performance sample

--- samples/src/instrumentation-performance.cpp
+++ samples/src/instrumentation-performance.cpp
@@ -41,19 +41,104 @@
     #define EZP_SAMPLE_END EZP_END_OFFLINE
 #endif
 
+#define EZP_SAMPLE_MEASURE_BEGIN    clock_gettime(CLK,&t1);
+#define EZP_SAMPLE_MEASURE_END      clock_gettime(CLK,&t2); t = (t2.tv_sec - t1.tv_sec)*1000000000LL + t2.tv_nsec - t1.tv_nsec;
+#define EZP_META_MEASURE_BEGIN      clock_gettime(CLK,&mt1);
+#define EZP_META_MEASURE_END        clock_gettime(CLK,&mt2); mt = (mt2.tv_sec - mt1.tv_sec)*1000000000LL + mt2.tv_nsec - mt1.tv_nsec;
+
+const int nsamples = 1000;
+const int nblocks = 1000;
+const int omsamples = 10;
+const int dsamples = 10;
+
+struct timespec t1,t2,mt1,mt2;
+char cbuf[5];
+unsigned long long int avgstarttimes[nblocks];
+unsigned long long int avgendtimes[nblocks];
+unsigned long long int minstarttime[nblocks];
+unsigned long long int minendtime[nblocks];
+unsigned long long int maxstarttime[nblocks];
+unsigned long long int maxendtime[nblocks];
+unsigned long long int t,mt;
+unsigned long long int overhead = UINT_MAX;
+
+inline void measureAndDiscard(const char* argv_0){
+    for(int s=0;s<dsamples;s++){
+        for(int i=0;i<nsamples;i++)
+            for(int b=0;b<nblocks;b++){
+                sprintf(cbuf,"%d",b);
+
+                EZP_SAMPLE_MEASURE_BEGIN
+                EZP_SAMPLE_START(cbuf)
+                EZP_SAMPLE_MEASURE_END
+
+                EZP_SAMPLE_MEASURE_BEGIN
+                EZP_SAMPLE_END(cbuf)
+                EZP_SAMPLE_MEASURE_END
+
+            }
+
+        printf("%s: Intermediate measurement no.%d, discarding\n", argv_0, s);
+    }
+}
+
+inline void measure(){
+    for(int i=0;i<nsamples;i++)
+        for(int b=0;b<nblocks;b++){
+            sprintf(cbuf,"%d",b);
+
+            EZP_SAMPLE_MEASURE_BEGIN
+            EZP_SAMPLE_START(cbuf)
+            EZP_SAMPLE_MEASURE_END
+
+            if(t < overhead)
+                t = 0;
+            else
+                t -= overhead;
+            avgstarttimes[b] += t;
+            if(t > maxstarttime[b])
+                maxstarttime[b] = t;
+            if(t < minstarttime[b])
+                minstarttime[b] = t;
+
+            EZP_SAMPLE_MEASURE_BEGIN
+            EZP_SAMPLE_END(cbuf)
+            EZP_SAMPLE_MEASURE_END
+
+            if(t < overhead)
+                t = 0;
+            else
+                t -= overhead;
+            avgendtimes[b] += t;
+            if(t > maxendtime[b])
+                maxendtime[b] = t;
+            if(t < minendtime[b])
+                minendtime[b] = t;
+        }
+}
+
+__attribute__((optimize("unroll-loops"))) inline unsigned long long int getMeasurementOverhead(){
+
+    //We assume that placing one clock_gettime() before and one clock_gettime()
+    //after the measured block will incur approximately one clock_gettime()
+    //overhead, so one clock_gettime() overhead will be subtracted from
+    //measurements
+
+    EZP_META_MEASURE_BEGIN
+
+    for(int i=0;i<nsamples*nblocks;i++){
+        clock_gettime(CLK,&t1);
+        clock_gettime(CLK,&t2);
+        clock_gettime(CLK,&t1);
+        clock_gettime(CLK,&t2);
+    }
+
+    EZP_META_MEASURE_END
+
+    return mt/4/(nsamples*nblocks);
+}
+
 int main(int argc, char** argv){
-    const int nsamples = 1000;
-    const int nblocks = 1000;
-
-    struct timespec t1,t2;
-    char cbuf[5];
-    unsigned long long int avgstarttimes[nblocks];
-    unsigned long long int avgendtimes[nblocks];
-    unsigned long long int minstarttime[nblocks];
-    unsigned long long int minendtime[nblocks];
-    unsigned long long int maxstarttime[nblocks];
-    unsigned long long int maxendtime[nblocks];
-
     for(int b=0;b<nblocks;b++){
         avgstarttimes[b] = 0;
         avgendtimes[b] = 0;
@@ -63,36 +148,33 @@ int main(int argc, char** argv){
         maxendtime[b] = 0;
     }
 
-    printf("%s: Beginning instrumentation performance measurement\n", argv[0]);
-
     EZP_ENABLE
 
-    //Measure nblocks many blocks, nsamples times each
-    unsigned int t;
-    for(int i=0;i<nsamples;i++)
-        for(int b=0;b<nblocks;b++){
-            sprintf(cbuf,"%d",b);
+    //
+    //Measure measurement overhead
+    //
 
-            clock_gettime(CLK,&t1);
-            EZP_SAMPLE_START(cbuf)
-            clock_gettime(CLK,&t2);
-            t = (t2.tv_sec - t1.tv_sec)*1000000000l + t2.tv_nsec - t1.tv_nsec;
-            avgstarttimes[b] += t;
-            if(t > maxstarttime[b])
-                maxstarttime[b] = t;
-            if(t < minstarttime[b])
-                minstarttime[b] = t;
+    printf("%s: Measuring measurement overhead, will collect %d samples and take minimum\n", argv[0], omsamples);
 
-            clock_gettime(CLK,&t1);
-            EZP_SAMPLE_END(cbuf)
-            clock_gettime(CLK,&t2);
-            t = (t2.tv_sec - t1.tv_sec)*1000000000l + t2.tv_nsec - t1.tv_nsec;
-            avgendtimes[b] += t;
-            if(t > maxendtime[b])
-                maxendtime[b] = t;
-            if(t < minendtime[b])
-                minendtime[b] = t;
-        }
+    //Taking minimum of 10 overhead measurements
+    for(int i=0;i<omsamples;i++){
+        unsigned long long int p = getMeasurementOverhead();
+        printf("%s: Intermediate overhead measurement no.%d: %lld ns\n", argv[0], i, p);
+        if(p < overhead)
+            overhead = p;
+    }
+
+    printf("%s: Measurement overhead is measured as %lld ns\n", argv[0], overhead);
+
+    //
+    //Measure instrumentation performance
+    //
+
+    printf("%s: Beginning instrumentation performance measurement, will sample and discard %d sets first\n", argv[0], dsamples);
+
+    measureAndDiscard(argv[0]);
+
+    measure();
 
     //Print statistics
     printf("%s: ==============================================\n", argv[0]);
